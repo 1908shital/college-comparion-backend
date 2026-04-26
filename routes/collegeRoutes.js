b69a3a071ce11147190d14713031e9cfbfa9f40d
1908shital <1908shitalchaudhari@gmail.com>
2026-04-26 17:37:00 +0530

    Updated backend routes and filters

--- routes/collegeRoutes.js
+++ routes/collegeRoutes.js
@@ -1,5 +1,34 @@
+// const express = require("express");
+// const router = express.Router();
+// const {
+//     addCollege,
+//     getColleges,
+//     filterColleges,
+// } = require("../controllers/collegeController");
+
+// const { body } = require("express-validator");
+
+// router.post(
+//     "/add",
+//     [
+//         body("name").notEmpty().withMessage("College name required"),
+//         body("fees").isNumeric().withMessage("Fees must be number"),
+//         body("location").notEmpty().withMessage("Location required"),
+//         body("rating")
+//             .isFloat({ min: 1, max: 5 })
+//             .withMessage("Rating should be between 1 and 5"),
+//     ],
+//     addCollege,
+// );
+
+// router.get("/", getColleges);
+// router.get("/filter", filterColleges);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
+
 const {
     addCollege,
     getColleges,
@@ -21,7 +50,17 @@ router.post(
     addCollege,
 );
 
-router.get("/", getColleges);
+// Updated GET route
+router.get("/", (req, res) => {
+    const { location, minRating, maxFees } = req.query;
+
+    if (location || minRating || maxFees) {
+        return filterColleges(req, res);
+    }
+
+    return getColleges(req, res);
+});
+
 router.get("/filter", filterColleges);
 
 module.exports = router;
